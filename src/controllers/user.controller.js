173d140c3e1d639b71b6fb75bf3d20ada3b1a2e8
Atharva-Thokal <thokalatharva99@gmail.com>
2024-04-01 23:41:07 +0530

    Add subcription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -190,7 +190,7 @@ const logoutUser = asyncHandler(async(req, res) => {
 
 const refereshAccessToken = asyncHandler( async (req, res) =>{
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
         throw new ApiError(401, "Unauthorized request")
     }
 
---
+++ src/models/subcriptions.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from "mongoose";
+
+const subcriptionSchema = new Schema({
+    subcriber: {
+        type: Schema.Types.ObjectId,// one who has subcribed
+        ref: "User"
+    },
+    channal: {
+        type: Schema.Types.ObjectId,//one whom subcriber is subcribing
+        ref: "User"
+    }
+}, {timestamps: true})
+
+export const Subcription = mongoose.model("Subcription", subcription)
