ab1d4ee01362d7922fe30f5e3a0c7ae5d2aa56ca
Atharva-Thokal <thokalatharva99@gmail.com>
2024-04-09 19:06:29 +0530

    fix coverImage route bug

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -41,7 +41,7 @@ router.route("/change-password").post(verifyJWT, changeUserPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)//here patch is used as only some fields are updated not all fields
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 router.route("/c:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
 
